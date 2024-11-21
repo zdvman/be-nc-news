9f7465edd726d92ec27f2d5b26b3a3c1a6108e42
Hev <150343854+nc-hev-mag@users.noreply.github.com>
2024-11-21 13:14:56 +0000

    remove trailing /

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,10 +3,10 @@ const endpointsJson = require("../endpoints.json");
 
 /* Set up your beforeEach & afterAll functions here */
 
-describe("GET /api/", () => {
+describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
-      .get("/api/")
+      .get("/api")
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
